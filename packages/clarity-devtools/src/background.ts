54ccd0fbb9166d620ec1a913ede2ee11a4c235fa
ken90242 <ken90242@gmail.com>
2022-02-17 16:12:31 -0800

    f21-research

--- packages/clarity-devtools/src/background.ts
+++ packages/clarity-devtools/src/background.ts
@@ -1,49 +1,30 @@
 let connections: any[] = [];
+let settingsSent: boolean;
 
-chrome.runtime.onConnect.addListener(function(port: chrome.runtime.Port): void {
 
-    let listener = function(message: any): void {
-        // Store a reference to tabId of the devtools page and send a message to activate Clarity
-        if (message.action === "init") {
-          connections[message.tabId] = port;
-          chrome.tabs.sendMessage(message.tabId, {action: "activate"});
-          return;
-        } else if (message.action === "warn") {
-          chrome.tabs.sendMessage(message.tabId, {action: "warn", message: message.message});
-          return;
-        }
-    };
-
-    // Listen to messages sent from the devtools page
-    port.onMessage.addListener(listener);
-
-    port.onDisconnect.addListener(function(): void {
-        port.onMessage.removeListener(listener);
-        let tabs = Object.keys(connections);
-        for (let tab of tabs) {
-          if (connections[tab] === port) {
-            delete connections[tab];
-            break;
-          }
-        }
-    });
-});
+chrome.commands.onCommand.addListener((command) => {
+  if (command == "switch-recording") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "switch-recording" })
+    })
+  }
+})
 
 // Receive message from content script and relay it to the devtools page
 chrome.runtime.onMessage.addListener(
-    function(message: any, sender: chrome.runtime.MessageSender, sendResponse: (response: any) => void): void {
+    function(message: any, sender: chrome.runtime.MessageSender, sendResponse: (response: any) => void): boolean {
         switch (message.action) {
           case "activate":
             if (sender.tab) {
               let tabId = sender.tab.id;
-              let success = tabId in connections;
+              let success = tabId in connections && settingsSent;
               let icon = success ? "icon-activated.png" : "icon.png";
               let title = success ? "Clarity Developer Tools" : "Clarity Developer Tools: Open developer tools to activate";
               chrome.browserAction.setIcon({ path: icon, tabId });
               chrome.browserAction.setTitle({ title, tabId });
               sendResponse({ success });
             }
-            break;
+            return true;
           case "payload":
             if (sender.tab) {
               let tabId = sender.tab.id;
@@ -53,7 +34,36 @@ chrome.runtime.onMessage.addListener(
               }
               sendResponse({ success });
             }
-            break;
+            return true;
+          case 'init':
+            if (sender.tab) {
+              let tabId = sender.tab.id;
+              connections[tabId] = 'init';
+
+              const url = new URL(sender.tab.url);
+              const domain = url.hostname;
+
+              fetch(`http://localhost:80/settings?domain=${domain}`,
+                  { headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }).then((res) => {
+                settingsSent = true;
+                const success = tabId in connections;
+                res.json().then(({ settings }) => {
+                  sendResponse({ success, settings });
+                })
+              });
+            }
+            return true;
+          case 'warn':
+            if (sender.tab) {
+              let tabId = sender.tab.id;
+              let success = tabId in connections;
+              chrome.tabs.sendMessage(tabId, { action: "warn", message: message.message });
+              sendResponse({ success });
+              
+            }
+            return true;
+          default:
+            return true;
         }
     }
 );
--- packages/clarity-devtools/src/content.ts
+++ packages/clarity-devtools/src/content.ts
@@ -1,21 +1,175 @@
 import config from "./config";
 
-chrome.runtime.onMessage.addListener(function(message: any): void {
+let taskSettings;
+
+chrome.runtime.sendMessage({ action: "init" }, function({ settings }) {
+  taskSettings = settings;
+  activate();
+});
+
+chrome.runtime.onMessage.addListener(function (message) {
   if (message.action === "activate") {
     activate();
   } else if (message.action === "warn") {
     console.warn(message.message);
+  } else if (message.action == "switch-recording") {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:80/switch-recording", true);
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.send();
   }
+  return true;
 });
 
-chrome.runtime.sendMessage({ action: "activate" }, function(response: any): void {
-  if (response && response.success) {
-    activate();
+function getCurrentTaskNum() {
+  if (!localStorage.getItem('currentTask')) {
+    return null;
   }
+  return parseInt(localStorage.getItem('currentTask'));
+}
+
+function loadTaskComponents() {
+  const buttonMenu = document.createElement("div");
+  buttonMenu.className = 'during-task-block';
+
+  const helpButton = document.createElement("div");
+  helpButton.className = 'task-help';
+  helpButton.textContent = '?';
+  helpButton.onclick = () => promptHelp(getCurrentTaskNum());
+
+  const finishTaskButton = document.createElement("div");
+  finishTaskButton.className = 'finish-task-button';
+  finishTaskButton.textContent = 'Finish Task';
+  finishTaskButton.onclick = () => {
+    const confirmEnd = confirm('Are you sure you want to finish this task?');
+    if (confirmEnd) {
+      endTask(buttonMenu);
+    }
+  }
+
+  buttonMenu.appendChild(helpButton);
+  buttonMenu.appendChild(finishTaskButton);
+  document.body.appendChild(buttonMenu);
+
+  // fetch(chrome.runtime.getURL('/menu.html')).then(r => r.text()).then(html => {
+    // document.body.insertAdjacentHTML('beforeend', html);
+    
+    // not using innerHTML as it would break js event listeners of the page
+  // });
+}
+
+function removeLightbox() {
+  const lb = document.getElementById('lightbox_background');
+  lb.parentNode.removeChild(lb);
+}
+
+function endTask(buttonMenu) {
+  sendTaskEndSignal();
+  buttonMenu.parentNode.removeChild(buttonMenu);
+  const nextTaskNum = getCurrentTaskNum() + 1;
+  if (nextTaskNum <= taskSettings.length) {
+    localStorage.setItem('currentTask', nextTaskNum.toString());
+    promptTask(getCurrentTaskNum());
+  }
+}
+
+function beginTask() {
+  removeLightbox();
+  sendTaskStartSignal();
+  loadTaskComponents();
+}
+
+function newTaskBlock({ taskNum, taskTitle, scenario, taskDesc, button }) {
+  const taskBlock = document.createElement("div");
+  taskBlock.className = "task-block";
+  const taskHeader = document.createElement("div");
+  const headSpanOne = document.createElement("div");
+  const headSpanTwo = document.createElement("div");
+  headSpanOne.textContent = `Task ${taskNum} out of ${taskSettings.length}`;
+  headSpanTwo.textContent = taskTitle;
+
+  taskHeader.className = "task-block-header"
+  taskHeader.appendChild(headSpanOne);
+  taskHeader.appendChild(headSpanTwo);
+  const p = document.createElement('p');
+  p.innerHTML = `You are about begin task ${taskNum} of the usability study<br/><br/> \
+  <u>Scenario</u>: ${scenario}<br/><br/> \
+  <u>Task:</u> ${taskDesc}<br/><br/> \
+  Please click on <b>Begin Task</b> to start the task, and click on <b>Finish Task</b> button to end the task.<br/><br/> \
+  (If you want to view the task descriptions again during the test, click on <b>?</b> button.)`;
+  const taskBlockCenter = document.createElement("div");
+  taskBlockCenter.className = "task-block-center";
+  taskBlockCenter.appendChild(p);
+  const taskBlockFooter = document.createElement("div");
+  taskBlockFooter.className = "task-block-footer";
+  taskBlockFooter.appendChild(button);
+  taskBlock.appendChild(taskHeader);
+  taskBlock.appendChild(taskBlockCenter);
+  taskBlock.appendChild(taskBlockFooter);
+  return taskBlock;
+}
+
+function createLightbox() {
+  const background = document.createElement('div');
+  background.id = "lightbox_background";
+  const lightbox = document.createElement('div');
+  lightbox.id = "lightbox";
+  
+  document.body.appendChild(background);
+  background.appendChild(lightbox);
+  return background;
+}
+
+function promptHelp(taskNum) {
+  const background = createLightbox();
+  const taskSetting = taskSettings[taskNum - 1]
+  const returnButton = document.createElement('span');
+  returnButton.onclick = removeLightbox;
+  returnButton.textContent='Return';
+
+  const taskBlock = newTaskBlock({
+    taskNum,
+    button: returnButton,
+    ...taskSetting,
+  });
+  
+  background.appendChild(taskBlock);
+}
+
+function promptTask(taskNum) {
+  const background = createLightbox();
+  const taskSetting = taskSettings[taskNum - 1];
+  
+  const beginButton = document.createElement('span');
+  beginButton.onclick = beginTask;
+  beginButton.textContent='Begin Task';
+
+  const taskBlock = newTaskBlock({
+    taskNum,
+    button: beginButton,
+    ...taskSetting,
+  });
+  
+  background.appendChild(taskBlock);
+}
+
+chrome.runtime.sendMessage({ action: "activate" }, function(response: any): void {
+  // if (response && response.success) {
+  //   activate();
+  // }
+  return response;
 });
 
 function activate(): void {
   setup(chrome.extension.getURL('clarity.js'));
+  // const curTaskNum = getCurrentTaskNum();
+  // if (curTaskNum == null) {
+    localStorage.setItem('currentTask', '1');
+    console.log(parseInt(localStorage.getItem('currentTask')))
+    promptTask(parseInt(localStorage.getItem('currentTask')));
+  // } else if (curTaskNum + 1 <= taskSettings.length) {
+  //   loadTaskComponents();
+  // }
 }
 
 function setup(url: string): void {
@@ -24,6 +178,15 @@ function setup(url: string): void {
   script.setAttribute('type', 'text/javascript');
   script.setAttribute('src', url);
   document.body.appendChild(script);
+  
+  window.alert = function(al){
+    return function(msg) {
+        al(msg);
+        console.log('aaaaaaabbbbbbbbccccccccc')
+        // alert("okbuttonclicked");
+    };
+}(window.alert);
+  window.alert('aaasd');
   
   window.addEventListener("message", function(event: MessageEvent): void {
       if (event.source === window && event.data.action) {
@@ -49,9 +212,7 @@ function setup(url: string): void {
             break;
         }
       }
-  });
-
-  
+  });  
 }
 
 function wireup(settings: any): string {
@@ -73,10 +234,24 @@ function wireup(settings: any): string {
   return `(${code})();`;
 }
 
+function sendTaskStartSignal(): void {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:80/task-start", true);
+  xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+  xhr.send();
+}
+
+function sendTaskEndSignal(): void {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:80/task-end", true);
+  xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+  xhr.send();
+}
+
 function upload(data: string): void {
-  chrome.runtime.sendMessage({ action: "payload", payload: data }, function(response: any): void {
-    if (!(response && response.success)) {
-      console.warn("Payload failure, dev tools likely not open.");
-    }
-  });
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:80", true);
+  xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+  console.log(data)
+  xhr.send(data);
 }
